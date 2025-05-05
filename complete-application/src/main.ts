c555cf14f18b14692b4cc233e03e862d1f1ed7b1
Mark Robustelli <137117976+mark-robustelli@users.noreply.github.com>
2025-05-05 16:34:18 -0700

    Update main.ts

--- complete-application/src/main.ts
+++ complete-application/src/main.ts
@@ -10,7 +10,7 @@ const app = createApp(App)
 
 app.use(FusionAuthVuePlugin, {
   clientId: 'ec526002-35cc-4e6e-8f5b-0e4fba2b08c8',
-  serverUrl: 'https://authcbchange.codegremlins.net',
+  serverUrl: 'https://authcbchangebad.codegremlins.net',
   redirectUri: 'https://changebank.codegremlins.net',
   postLogoutRedirectUri: 'https://changebank.codegremlins.net',
   shouldAutoFetchUserInfo: true,
